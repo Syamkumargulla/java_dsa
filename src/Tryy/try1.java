6036164444389b60e8a7664fef873a7eda2c7762
GULLA.SYAMKUMAR <syamk2424@gmail.com>
2026-01-28 20:52:29 +0530

    encap2

--- src/Tryy/try1.java
+++ src/Tryy/try1.java
@@ -6,5 +6,14 @@ public class try1 {
     public static void main(String[] args) {
         int n =5;
 
+        for(int i = 0; i< n; i++) {
+
+            for (int j = 0; j < i; j++) {
+                if (j % 2 != 0) {
+                    System.out.print("*");
+                }}
+                System.out.println();
+//.
+        }
     }
 }
